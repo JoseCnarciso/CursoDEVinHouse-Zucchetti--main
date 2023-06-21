8b980bfbdd1aeb901a22d90bf676c4ba9437a43c
JoseCnarciso <josecdia@hotmail.com>
2023-06-20 22:07:16 -0300

    linkando html no java script

---
+++ Modulo-01/quartaSemana/Exercicio-Aula/script.js
@@ -0,0 +1,14 @@
+console.log("cheguei")
+
+function clicarBotao(){
+
+var email = document.getElementById("campo-email").value
+var senha = document.getElementById("campo-senha").value
+
+if(email === ""){
+    alert("email invalido")
+}
+
+console.log(email)
+console.log(senha)
+}
