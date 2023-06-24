3f290dc0bd528cf6c7407e31a3088021be635ead
JoseCnarciso <josecdia@hotmail.com>
2023-06-23 23:53:14 -0300

    Exercicio refatorado

--- Modulo-01/quartaSemana/calculadoraFgts/script.js
+++ Modulo-01/quartaSemana/calculadoraFgts/script.js
@@ -1,12 +1,15 @@
+// Esta função pega os dados do iinput do salário pelo id
 function calcularValorMensal() {
     var salarioBruto = document.getElementById('input-valor-salario').value;
     var fgts = salarioBruto * 0.08;
+    // retorna o valor do fgts 
     return fgts;
 }
-
+// pega o input da quantidade de meses contribuidos
 function calcularQuantidadeMeses() {
     var valorMensal = calcularValorMensal();
     var valorTotalDeMeses = document.getElementById('input-qtd-meses').value;
+    // retorna o valor total
     return valorMensal * valorTotalDeMeses;
 }
 
@@ -14,7 +17,9 @@ function valorTotal() {
     var totalArrecadado = calcularQuantidadeMeses();
     var valorMensal = calcularValorMensal().toFixed(2);
     var valorAcumulado = totalArrecadado.toFixed(2);
-
+    // Pega o id da tabela e insere os dados em tela 
     document.getElementById('valorMensal').textContent = 'R$ ' + valorMensal;
     document.getElementById('valorAcumulado').textContent = 'R$ ' + valorAcumulado;
+    document.getElementById('text-valor-final').textContent = 'Montante acumulado R$: ' + valorAcumulado
 }
+
