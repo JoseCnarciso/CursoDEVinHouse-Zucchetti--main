4dd14acdccc34b13f253929f8abac8374171320a
JoseCnarciso <josecdia@hotmail.com>
2023-06-26 23:58:37 -0300

    exercicio 1

---
+++ Modulo-01/quintaSemana/AulaDia26/main.js
@@ -0,0 +1,40 @@
+console.log("m1s05a1");
+
+// exemplo
+// f(x, y) = x * 2 - y
+// f(2, 3) = 2 * 2 - 3
+// f(2, 3) = 4 - 3
+// f(2, 3) = 1
+
+function f(x, y) {
+  const resultado = x * 2 - y;
+  return resultado;
+}
+
+const retorno = f(2, 3);
+console.log(retorno);
+
+// exemplo função efemera
+(function () {
+  console.log("Funcao de uso unico!");
+})();
+
+// comparação arrow function
+function funcaoComum(a) {
+  return "Funcao comum. " + a;
+}
+
+console.log(funcaoComum("José"));
+
+const funcaoArrow = (a) => {
+  return "Funcao arrow. " + a;
+};
+// equivalente a
+//const funcaoArrow = a => "Funcao arrow. " + a;
+
+console.log(funcaoArrow("Narciso"));
+
+
+(function (){
+  console.log("Exemplo de uso uníco")
+})();
---
+++ Modulo-01/quintaSemana/exercicios/classificaIdade/main.js
@@ -0,0 +1,17 @@
+const informarIdade = alert("Qual a sua idade");
+
+
+function classificarIdade(){
+    let idade = document.getElementById('input-idade').value;
+
+    if(!idade ){
+        return alert('Favor digitar sua idade');
+    }else if(idade <=15){
+        return alert("Pessoa Jovem");
+    }else if(idade <=64){
+        return alert('Pessoa Adulta');
+    }else if(idade >= 65){
+        return alert("Pessoa Idosa");
+    }
+
+}
