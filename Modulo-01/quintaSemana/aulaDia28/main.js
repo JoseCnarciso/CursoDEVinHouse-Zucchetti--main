adf86e05ff7bae55dbc8a0f6dd3b007e2f3057c7
JoseCnarciso <josecdia@hotmail.com>
2023-06-28 19:46:17 -0300

    Exercicio aulas

---
+++ Modulo-01/quintaSemana/aulaDia28/main.js
@@ -0,0 +1,23 @@
+const elementoh1 = document.getElementById('titulo');
+
+console.log(elementoh1);
+
+// Estruturas condicionais e estruturas de repetição
+
+const idade = 0;
+
+function validardidade(){
+    if(idade >= 18){
+        console.log("Maior de idade");
+    }else{
+        console.log("Menor de idade");
+    }
+}
+
+validardidade(17);
+
+
+function digitou(){
+    const valor = campo.valor();
+}
+
