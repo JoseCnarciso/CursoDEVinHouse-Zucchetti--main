62ddd9c51e0d3235570a68ddd454a7b754993a70
JoseCnarciso <josecdia@hotmail.com>
2023-06-29 18:59:43 -0300

    aula dia 28 finalizada

--- Modulo-01/quintaSemana/aulaDia28/main.js
+++ Modulo-01/quintaSemana/aulaDia28/main.js
@@ -4,6 +4,24 @@ console.log(elementoh1);
 
 // Estruturas condicionais e estruturas de repetição
 
+// Valores equivalentes a FALSO
+// false, 0, "", null, undefined, NaN
+
+// Valores equivalentes a TRUE
+// true, numeros(positivos ou negativos), "abc", [],{} 
+
+// Exemplo estruturas condicionais
+// if SIMPLES
+
+
+let valor = 0;
+if(valor <= 100 ){
+    console.log("Tô pobre");
+}
+
+// Exemplo de estruturas condicional 
+// else 
+
 const idade = 0;
 
 function validardidade(){
@@ -16,8 +34,57 @@ function validardidade(){
 
 validardidade(17);
 
-
-function digitou(){
-    const valor = campo.valor();
+// equivalente em switch
+const numero = 1;
+switch(numero ){
+    case 0: 
+    console.log("zero");
+    break;
+    case 1: console.log("um");
+    break;
+    case 2: console.log("dois");
+    break;
+    default: console.log("Numero errado");
 }
 
+// Estruturas de repetição
+// Exemplo for
+//({inicialização}{condiçoa}{incremento})
+for(let i = 0;    i < 5;    i++){
+    console.log("Numero = " + i)
+}
+
+const listaA=["banana","limao", "pereira"]
+
+for(let i = 0;    i < listaA.length;    i++){
+    console.log(listaA[i])
+}
+
+// while
+// let entrada ="";
+// while(entrada !== "fim"){
+//    let entrada = prompt("Digite fim");
+//     console.log(entrada);
+// }
+
+// Exemplo forEach
+const lista2 =[1,2,4,3,5,6,8,7];
+
+lista2.push("Jabiticaba pretissima")
+//imprime todos os itens da lista2
+// lista2.forEach((item,indice,lista) =>{
+//     console.log(item,indice,lista === listaA)
+// });
+
+// SPREAD com vetores
+const vertoA = [1,2,3];
+const vetorB = [4,5,6];
+const vetorC = [...vetorB,...vertoA];
+
+console.log(vetorC);
+
+// SPREAD com objetos
+const objetoA = {a:1,b:2,c:3};
+const objetoB = {d:24,e:25,f:26};
+const objetoC ={...objetoB,...objetoA}
+console.log(objetoC);
